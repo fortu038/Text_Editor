cb318a0577dd04c60c60dc7dfa266c87eb9a555f
fortu038 <fortu038@umn.edu>
2022-11-13 20:35:22 -0600

    updated getDb and putDb in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,37 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Note: Parts of the putDb and getDb code were provided by the instructor
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  console.log(">>>PUT to the database");
+
+  const jateDb = await openDB("jate", 1);
+
+  const tx = jateDb.transaction("jate", "readwrite");
+
+  const store = tx.objectStore("jate");
+
+  const request = store.put({ id: 1, value: content });
+
+  const result = await request;
+  console.log(">>>Data saved to database", result.value);
+}
+
+export const getDb = async () => {
+  console.log(">>>GET from the database");
+
+  const jateDb = await openDB("jate", 1);
+
+  const tx = jateDb.transaction("jate", "readonly");
+
+  const store = tx.objectStore("jate");
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log(">>>Got all data from database", result.value);
+  return result;
+};
 
 initdb();
